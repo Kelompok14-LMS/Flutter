6d0bfa11fc601bb6081294b65d9c74415c30d696
naufalrays <rayhannaufalherlano@gmail.com>
2022-11-22 20:03:10 +0700

    feature: add bottom navigation bar

---
+++ lib/view/course/course_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CourseScreen extends StatelessWidget {
+  const CourseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test'),
+      ),
+      body: const Center(
+        child: Text('Center'),
+      ),
+    );
+  }
+}
