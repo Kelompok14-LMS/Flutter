1fa6ddbdcba05c975878d3f176941b52ff551cce
naufalrays <rayhannaufalherlano@gmail.com>
2022-11-22 15:50:29 +0700

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyColor {}
+
+const MaterialColor primarySwatch = MaterialColor(0xFF112D4E, <int, Color>{
+  50: Color(0xFFE2E6EA),
+  100: Color(0xFFB8C0CA),
+  200: Color(0xFF8896A7),
+  300: Color(0xFF586C83),
+  400: Color(0xFF354D69),
+  500: Color(0xFF112D4E),
+  600: Color(0xFF0F2847),
+  700: Color(0xFF0C223D),
+  800: Color(0xFF0A1C35),
+  900: Color(0xFF051125),
+});
+
+Color primary = const Color(0xff112D4E);
+// Color secondary = const Color.fromARGB(255, 1, 145, 193);
+// Color transparent = Colors.transparent;
+// Color black = Colors.black;
+// Color grey = Colors.grey;
+// Color red = Colors.red;
+// Color green = Colors.green;
+// Color shadow = Colors.grey.shade400;
+// Color textGrey1 = Colors.grey.shade500;
+// Color star = Colors.yellow.shade600;
