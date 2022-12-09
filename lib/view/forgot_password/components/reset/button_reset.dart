2386b09192d633728e2188a13dfa371f3f29482e
Gilang Fitra Ramadhana <gifrad98@gmail.com>
2022-12-09 14:41:47 +0700

    implementasi api forgot password

--- lib/view/forgot_password/components/reset/button_reset.dart
+++ lib/view/forgot_password/components/reset/button_reset.dart
@@ -1,34 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../view_models/auth_view_model.dart';
 import '../../../login/login_screen.dart';
+import '../../reset_passsword_screen.dart';
 
-class ButtonReset extends StatelessWidget {
+class ButtonReset extends StatefulWidget {
   const ButtonReset({
     Key? key,
     required this.size,
     required this.formKey,
-  }) : super(key: key);
+    required this.widget,
+    required TextEditingController passController,
+    required TextEditingController confirmController,
+  })  : _passController = passController,
+        _confirmController = confirmController,
+        super(key: key);
 
   final Size size;
   final GlobalKey<FormState> formKey;
+  final ResetPasswordScreen widget;
+  final TextEditingController _passController;
+  final TextEditingController _confirmController;
 
+  @override
+  State<ButtonReset> createState() => _ButtonResetState();
+}
+
+class _ButtonResetState extends State<ButtonReset> {
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       child: SizedBox(
-        width: size.width * 0.93,
-        height: size.height * 0.07,
+        width: widget.size.width * 0.93,
+        height: widget.size.height * 0.07,
         child: ElevatedButton(
-          onPressed: () {
-            if (formKey.currentState!.validate()) {
-              formKey.currentState!.save();
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const LoginScreen(),
-                  ),
-                  (route) => false);
+          onPressed: () async {
+            if (widget.formKey.currentState!.validate()) {
+              widget.formKey.currentState!.save();
+              final result = await context.read<AuthViewModel>().forgotPassword(
+                    widget.widget.email,
+                    widget._passController.text,
+                    widget._confirmController.text,
+                    widget.widget.otp,
+                  );
+              if (result == 'Success reset password') {
+                if (mounted) {
+                  print(widget.widget.email);
+                  print(widget.widget.otp);
+                  print(widget._passController.text);
+                  print(widget._confirmController.text);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(result!),
+                    ),
+                  );
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginScreen(),
+                    ),
+                    (route) => false,
+                  );
+                }
+              } else if (result != 'Success reset password') {
+                if (mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(result!),
+                    ),
+                  );
+                }
+              }
             }
           },
           child: const Text('Masukkan'),
