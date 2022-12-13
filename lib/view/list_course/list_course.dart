d546aa7bd0e3c236773260b6237771609f327346
meisyaamanda <meisyaaldiamanda@gmail.com>
2022-12-13 23:14:10 +0700

    push rank

--- lib/view/list_course/list_course.dart
+++ lib/view/list_course/list_course.dart
@@ -1,5 +1,9 @@
 import 'package:edu_world/utils/constant.dart';
+import 'package:edu_world/view/list_course/widget/course/kelas_course_home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../view_models/couse_view_model.dart';
 
 class Course extends StatefulWidget {
   const Course({super.key});
@@ -11,23 +15,48 @@ class Course extends StatefulWidget {
 class _CourseState extends State<Course> {
   @override
   Widget build(BuildContext context) {
+    final courseClassViewModel = Provider.of<CourseViewModel>(context);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            'Daftar Kursus',
-            style: MyColor().appTextsStyle,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: Text(
+          'Daftar Kursus',
+          style: MyColor().appTextsStyle,
+        ),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: MyColor.primary,
           ),
-          leading: IconButton(
-              onPressed: () => Navigator.pop(context),
-              icon: const Icon(
-                Icons.arrow_back_ios,
-                color: MyColor.primary,
-              ))),
+        ),
+      ),
       body: SafeArea(
         child: Column(
-          children: [],
+          children: [
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 28),
+              child: GridView.builder(
+                  itemCount: courseClassViewModel.allCourse.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8,
+                    crossAxisSpacing: 8,
+                    childAspectRatio: 0.9,
+                    mainAxisExtent: 224,
+                  ),
+                  itemBuilder: (context, index) {
+                    return KelasCourse(
+                        courseModel: courseClassViewModel.allCourse[index],
+                        fontSize: 16,
+                        height: 104);
+                  }),
+            ))
+          ],
         ),
       ),
     );
--- lib/view/recommendation/recommendation_view.dart
+++ lib/view/recommendation/recommendation_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:edu_world/utils/constant.dart';
 import 'package:flutter/material.dart';
 
@@ -13,79 +12,23 @@ class _RekomendasiState extends State<Rekomendasi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
+        bottomOpacity: 0.0,
+        elevation: 0.0,
         backgroundColor: Colors.white,
-        title: Text('Rekomendasi', style: MyColor().appTextsStyle),
+        title: Text(
+          'Kursus Populer',
+          style: MyColor().appTextsStyle,
+        ),
         leading: IconButton(
-          onPressed: () => Navigator.pop(context), 
-          icon: Icon(Icons.arrow_back_ios, color: MyColor.primary,),),
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: MyColor.primary,
+          ),
+        ),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    prefixIcon: Icon(Icons.search),
-                    prefixIconColor: MyColor.primary,
-                    hintText: 'Cari Course',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: MyColor.primary),
-                      onPressed: () {},
-                      child: Text('Semua Topik'),
-                    ),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: MyColor.primary),
-                      onPressed: () {},
-                      child: Text('UI/UX'),
-                    ),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: MyColor.primary),
-                      onPressed: () {},
-                      child: Text('Front End'),
-                    ),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: MyColor.primary),
-                      onPressed: () {},
-                      child: Text('Back End'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),),
     );
   }
 }
