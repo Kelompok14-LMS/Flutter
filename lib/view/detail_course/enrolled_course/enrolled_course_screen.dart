9f023ee1b34abd6d184d37ff6ac0953a86224b18
naufalrays <rayhannaufalherlano@gmail.com>
2022-12-23 09:55:33 +0700

    fix bug, button bug on android

--- lib/view/detail_course/enrolled_course/enrolled_course_screen.dart
+++ lib/view/detail_course/enrolled_course/enrolled_course_screen.dart
@@ -44,197 +44,215 @@ class _EnrolledCourseScreenState extends State<EnrolledCourseScreen> {
     super.initState();
   }
 
+  Future<bool> checkEnrollment(EnrollViewModel successEnrolled) {
+    if (widget.isHaveData || successEnrolled.addCourseSucces) {
+      Navigator.pop(context, true);
+      return Future.value(true);
+    } else {
+      Navigator.pop(context, false);
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final dataMaterials =
         Provider.of<MaterialsViewModel>(context, listen: false);
     final successEnrolled = Provider.of<EnrollViewModel>(context);
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () async {
-            if (widget.isHaveData || successEnrolled.addCourseSucces) {
-              Navigator.pop(context, true);
-            } else {
-              Navigator.pop(context, false);
-            }
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios_new_sharp,
-            color: Color(0xFF0C223D),
-          ),
-        ),
-        title: Text(
-          'Kembali',
-          style: GoogleFonts.roboto(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            color: const Color(0xff112D4E),
-          ),
-        ),
-      ),
-      body: widget.isHaveData || successEnrolled.addCourseSucces
-          ? SingleChildScrollView(
-              physics: const ClampingScrollPhysics(),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        HeaderModulCourse(courseModel: widget.courseModel),
-                        Consumer<MaterialsViewModel>(
-                          builder: (context, data, _) {
-                            if (data.courseMaterialsState ==
-                                CourseMaterialsState.loading) {
-                              return const ListCourseShimmer();
-                            }
-                            if (data.courseMaterialsState ==
-                                CourseMaterialsState.error) {
-                              return const Text('Gagal mengambil data');
-                            } else {
-                              if (data.errorMessage == "502") {
-                                return const Center(
-                                    child: Text(
-                                  'Error 502, gagal memuat data',
-                                  style: TextStyle(color: MyColor.primary),
-                                ));
-                              }
-                              if (data.modulsEnrolled.modules!.isNotEmpty) {
-                                return ListView.separated(
-                                    padding: EdgeInsets.zero,
-                                    physics:
-                                        const NeverScrollableScrollPhysics(),
-                                    shrinkWrap: true,
-                                    scrollDirection: Axis.vertical,
-                                    itemBuilder: (context, index) {
-                                      return Card(
-                                        clipBehavior:
-                                            Clip.antiAliasWithSaveLayer,
-                                        elevation: 2,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(16),
-                                        ),
-                                        child: Theme(
-                                            data: Theme.of(context).copyWith(
-                                                dividerColor:
-                                                    Colors.transparent),
-                                            child: SectionExpansionTile(
-                                              modules: dataMaterials
-                                                  .modulsEnrolled
-                                                  .modules![index],
-                                              index: index,
-                                              courseModel: widget.courseModel,
-                                              mentee: widget.mentee,
-                                            )),
-                                      );
-                                    },
-                                    separatorBuilder: (context, index) =>
-                                        const SizedBox(),
-                                    itemCount: dataMaterials
-                                        .modulsEnrolled.modules!.length);
-                              }
-                              return const Center(
-                                  child: Text('Tidak ada kursus'));
-                            }
-                          },
-                        ),
-                        Consumer<MaterialsViewModel>(
-                            builder: (context, value, child) {
-                          if (value.courseMaterialsState ==
-                              CourseMaterialsState.loading) {
-                            return Shimmer.fromColors(
-                              baseColor: Colors.grey.shade300,
-                              highlightColor: Colors.grey.shade500,
-                              loop: 3,
-                              child: Column(
-                                children: const [
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Skeleton(height: 50, width: double.maxFinite),
-                                ],
-                              ),
-                            );
-                          }
-                          if (value.courseMaterialsState ==
-                              CourseMaterialsState.none) {
-                            return dataMaterials.modulsEnrolled.assignment !=
-                                    null
-                                ? AssignmentExpansionTile(
-                                    dataMaterials: dataMaterials.modulsEnrolled,
-                                    isPreview: false,
-                                  )
-                                : Container();
-                          }
-                          return Container();
-                        })
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                ],
-              ),
-            )
-          : const Center(
-              child: Text('Gagal Menambahkan Kursus'),
-            ),
-      bottomNavigationBar: Consumer<MaterialsViewModel>(
-        builder: (context, value, _) {
-          if (value.courseMaterialsState == CourseMaterialsState.loading) {
-            return Shimmer.fromColors(
-                baseColor: Colors.grey.shade300,
-                highlightColor: Colors.grey.shade500,
-                loop: 3,
-                child: const Skeleton(height: 50, width: 50));
-          }
-          if (value.courseMaterialsState == CourseMaterialsState.error) {
-            return const Text('Error');
-          }
-          if (value.courseMaterialsState == CourseMaterialsState.none) {
-            if (value.modulsEnrolled.progress == null) {
-              return const CircularProgressIndicator();
-            } else {
-              final percent = (value.modulsEnrolled.progress! /
-                  value.modulsEnrolled.totalMaterials!);
-              final status = widget.courseModel.status ?? "completed";
-              if (percent >= 1.0 && status == "ongoing") {
-                return Container(
-                  height: 96,
-                  color: Colors.transparent,
-                  child: Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        showAlertDialogFunction();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: MyColor.primaryLogo,
-                      ),
-                      child: Text('Selesaikan Kursus',
-                          style: GoogleFonts.roboto(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                              color: MyColor.primary)),
-                    ),
-                  ),
-                );
+    return WillPopScope(
+      onWillPop: () async {
+        final shouldPop = await checkEnrollment(successEnrolled);
+        return shouldPop;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () async {
+              if (widget.isHaveData || successEnrolled.addCourseSucces) {
+                Navigator.pop(context, true);
               } else {
-                return const SizedBox();
+                Navigator.pop(context, false);
+              }
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new_sharp,
+              color: Color(0xFF0C223D),
+            ),
+          ),
+          title: Text(
+            'Kembali',
+            style: GoogleFonts.roboto(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: const Color(0xff112D4E),
+            ),
+          ),
+        ),
+        body: widget.isHaveData || successEnrolled.addCourseSucces
+            ? SingleChildScrollView(
+                physics: const ClampingScrollPhysics(),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          HeaderModulCourse(courseModel: widget.courseModel),
+                          Consumer<MaterialsViewModel>(
+                            builder: (context, data, _) {
+                              if (data.courseMaterialsState ==
+                                  CourseMaterialsState.loading) {
+                                return const ListCourseShimmer();
+                              }
+                              if (data.courseMaterialsState ==
+                                  CourseMaterialsState.error) {
+                                return const Text('Gagal mengambil data');
+                              } else {
+                                if (data.errorMessage == "502") {
+                                  return const Center(
+                                      child: Text(
+                                    'Error 502, gagal memuat data',
+                                    style: TextStyle(color: MyColor.primary),
+                                  ));
+                                }
+                                if (data.modulsEnrolled.modules!.isNotEmpty) {
+                                  return ListView.separated(
+                                      padding: EdgeInsets.zero,
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      shrinkWrap: true,
+                                      scrollDirection: Axis.vertical,
+                                      itemBuilder: (context, index) {
+                                        return Card(
+                                          clipBehavior:
+                                              Clip.antiAliasWithSaveLayer,
+                                          elevation: 2,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16),
+                                          ),
+                                          child: Theme(
+                                              data: Theme.of(context).copyWith(
+                                                  dividerColor:
+                                                      Colors.transparent),
+                                              child: SectionExpansionTile(
+                                                modules: dataMaterials
+                                                    .modulsEnrolled
+                                                    .modules![index],
+                                                index: index,
+                                                courseModel: widget.courseModel,
+                                                mentee: widget.mentee,
+                                              )),
+                                        );
+                                      },
+                                      separatorBuilder: (context, index) =>
+                                          const SizedBox(),
+                                      itemCount: dataMaterials
+                                          .modulsEnrolled.modules!.length);
+                                }
+                                return const Center(
+                                    child: Text('Tidak ada kursus'));
+                              }
+                            },
+                          ),
+                          Consumer<MaterialsViewModel>(
+                              builder: (context, value, child) {
+                            if (value.courseMaterialsState ==
+                                CourseMaterialsState.loading) {
+                              return Shimmer.fromColors(
+                                baseColor: Colors.grey.shade300,
+                                highlightColor: Colors.grey.shade500,
+                                loop: 3,
+                                child: Column(
+                                  children: const [
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Skeleton(
+                                        height: 50, width: double.maxFinite),
+                                  ],
+                                ),
+                              );
+                            }
+                            if (value.courseMaterialsState ==
+                                CourseMaterialsState.none) {
+                              return dataMaterials.modulsEnrolled.assignment !=
+                                      null
+                                  ? AssignmentExpansionTile(
+                                      dataMaterials:
+                                          dataMaterials.modulsEnrolled,
+                                      isPreview: false,
+                                    )
+                                  : Container();
+                            }
+                            return Container();
+                          })
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                  ],
+                ),
+              )
+            : const Center(
+                child: Text('Gagal Menambahkan Kursus'),
+              ),
+        bottomNavigationBar: Consumer<MaterialsViewModel>(
+          builder: (context, value, _) {
+            if (value.courseMaterialsState == CourseMaterialsState.loading) {
+              return Shimmer.fromColors(
+                  baseColor: Colors.grey.shade300,
+                  highlightColor: Colors.grey.shade500,
+                  loop: 3,
+                  child: const Skeleton(height: 50, width: 50));
+            }
+            if (value.courseMaterialsState == CourseMaterialsState.error) {
+              return const Text('Error');
+            }
+            if (value.courseMaterialsState == CourseMaterialsState.none) {
+              if (value.modulsEnrolled.progress == null) {
+                return const CircularProgressIndicator();
+              } else {
+                final percent = (value.modulsEnrolled.progress! /
+                    value.modulsEnrolled.totalMaterials!);
+                final status = widget.courseModel.status ?? "completed";
+                if (percent >= 1.0 && status == "ongoing") {
+                  return Container(
+                    height: 96,
+                    color: Colors.transparent,
+                    child: Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          showAlertDialogFunction();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: MyColor.primaryLogo,
+                        ),
+                        child: Text('Selesaikan Kursus',
+                            style: GoogleFonts.roboto(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                                color: MyColor.primary)),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const SizedBox();
+                }
               }
             }
-          }
 
-          return Container();
-        },
+            return Container();
+          },
+        ),
       ),
     );
   }
